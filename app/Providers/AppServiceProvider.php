5bdf05c87a69431b82d8fa55b0fd2388c0be5fb1
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-07 17:22:20 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,9 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Force HTTPS URLs when behind a proxy (localtunnel, ngrok, etc.)
-        if ($this->app->environment('production') || request()->header('X-Forwarded-Proto') === 'https') {
-            URL::forceScheme('https');
-        }
+        //
     }
 }
