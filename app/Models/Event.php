fa8d915caa48c754a1a4dfe08ce3f9ad38433f3d
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-03 10:48:28 +0800

    Create Event.php

---
+++ app/Models/Event.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    protected $fillable = [
+        'name',
+        'event_date',
+        'revolving_funds',
+        'notes',
+        'status',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'event_date' => 'date',
+            'revolving_funds' => 'decimal:2',
+        ];
+    }
+
+    // Relationship: Event has many fights
+    public function fights()
+    {
+        return $this->hasMany(Fight::class, 'event_name', 'name')
+            ->where('event_date', $this->event_date);
+    }
+}
